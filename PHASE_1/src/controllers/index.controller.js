c1b4ea9b60195727571ea5e53d9a277f50c552b4
AndrewWei-Colosseum <66073874+AndrewWei-Colosseum@users.noreply.github.com>
2022-03-14 00:15:42 +1100

    Delete PHASE_1 directory

--- PHASE_1/src/controllers/index.controller.js
+++
@@ -1,5 +0,0 @@
-const index = (req, res) => {
-  res.send('This is index Route');
-};
-
-export default index;
--- PHASE_1/src/routes/index.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import IndexController from '../controllers/index.controller.js'
-
-const indexRouter = express.Router();
-indexRouter.get('/', IndexController);
-
-export default indexRouter;
-

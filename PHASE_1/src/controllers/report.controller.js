e0498579a3f221f91754e1159cf13612afdd078a
samztz <1196572086@qq.com>
2022-03-16 16:05:19 +1100

    pull

--- PHASE_1/src/controllers/report.controller.js
+++ PHASE_1/src/controllers/report.controller.js
@@ -1,10 +1,51 @@
-import mainPageScraper from "../scraper/mainPageScraper.js";
-import detailsScraper from "../scraper/detailsScraper.js";
-import Country_city_scraper from "../scraper/country_city_scraper.js";
+// import mainPageScraper from "../scraper/mainPageScraper.js";
+// import detailsScraper from "../scraper/detailsScraper.js";
+// import Country_city_scraper from "../scraper/country_city_scraper.js";
 
-// return most recent report in default 30 days
+/**
+ * @swagger
+ * /report:
+ *  get:
+ *    tags: [Report]
+ *    summary: get all report from main page (default past 30 days)
+ *    responses:
+ *      '201':
+ *        description: Success returned
+ *        content:
+ *          appplication/json:
+ *            schema:
+ *              type: object
+ *            example:
+ *              _id: 61da409632c8196efc5dd789
+ *              email: ante@aol.net
+ *              firstName: Caleb Collins
+ *              lastName: Baxter Burt
+ *              dob: '1996-02-28T00:00:00.000Z'
+ *              phone: (06) 5828 5812
+ *              notification:
+ *                - email
+ *              address:
+ *                street: '245 George St'
+ *                city: 'Syndey'
+ *                state: 'NSW'
+ *                postcode: '2000'
+ *              gender: Male
+ *              totalSpent: 0
+ *              orderAccumulation: 0
+ *              __v: 0
+ *      '400':
+ *        description: Duplicate key
+ *        content:
+ *          appplication/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  exmaple: 'error message'
+ */
 const getReports = async (req, res) => {
-  const result = await mainPageScraper();
+  // const result = await mainPageScraper();
   return res.json(result);
 
 }

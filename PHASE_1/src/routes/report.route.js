87ea78eec876e304055beee6fe993b61fcdb9ecd
samztz <1196572086@qq.com>
2022-03-17 09:01:14 +1100

    modified route

--- PHASE_1/src/routes/report.route.js
+++ PHASE_1/src/routes/report.route.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { getReports, getReportsByCountryId, getReportsByCityName, getReportDetailById } from '../controllers/report.controller.js';
+import {  getReportsByQuery, getReportDetailById } from '../controllers/report.controller.js';
 
 const reportRouter = express.Router();
-reportRouter.get('/', getReports)
-reportRouter.get('/country/:countryId', getReportsByCountryId)
-reportRouter.get('/city/:cityName', getReportsByCityName)
-reportRouter.get('/detail/:id', getReportDetailById)
+reportRouter.get('/', getReportsByQuery);
+reportRouter.get('/detail/:id', getReportDetailById);
 
 export default reportRouter;

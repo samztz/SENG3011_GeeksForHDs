f837aa63f1469b8cf3ccf162095dd2d46c59a2c7
Mashira Farid <mashira.farid@student.unsw.edu.au>
2022-03-28 06:26:42 +1100

    got report generator working

--- PHASE_1/src/scraper/articleScraper.js
+++ PHASE_1/src/scraper/articleScraper.js
@@ -2,6 +2,7 @@ import puppeteer from 'puppeteer';
 import { Cluster } from 'puppeteer-cluster';
 import { Readability } from '@mozilla/readability';
 import { JSDOM } from 'jsdom';
+import reportGenerator from './reportGenerator.js';
 
 async function articleScraper(results) {
     let articles = [];
@@ -53,113 +54,8 @@ async function articleScraper(results) {
         
         // else can scrape from website 
         } else {
-            let Disease_and_syndrome_keywords = {
-                "disease": [
-                    "unknown" ,
-                    "other" ,
-                    "anthrax cutaneous" ,
-                    "anthrax gastrointestinous" ,
-                    "botulism" ,
-                    "brucellosis" ,
-                    "chikungunya" ,
-                    "cholera" ,
-                    "cryptococcosis" ,
-                    "cryptosporidiosis" ,
-                    "crimean-congo haemorrhagic fever" ,
-                    "dengue" ,
-                    "diphteria" ,
-                    "ebola haemorrhagic fever" ,
-                    "ehec (e.coli)" ,
-                    "enterovirus 71 infection" ,
-                    "influenza a/h5n1",
-                    "influenza a/h7n9",
-                    "influenza a/h9n2",
-                    "influenza a/h1n1",
-                    "influenza a/h1n2",
-                    "influenza a/h3n5",
-                    "influenza a/h3n2",
-                    "influenza a/h2n2",
-                    "hand, foot and mouth disease" ,
-                    "hantavirus" ,
-                    "hepatitis a",
-                    "hepatitis b",
-                    "hepatitis c",
-                    "hepatitis d",
-                    "hepatitis e",
-                    "histoplasmosis" ,
-                    "hiv/aids" ,
-                    "lassa fever" ,
-                    "malaria" ,
-                    "marburg virus disease" ,
-                    "measles" ,
-                    "mers-cov" ,
-                    "mumps" ,
-                    "nipah virus" ,
-                    "norovirus infection" ,
-                    "pertussis" ,
-                    "plague",
-                    "pneumococcus pneumonia",
-                    "poliomyelitis" ,
-                    "q fever" ,
-                    "rabies" ,
-                    "rift valley fever" ,
-                    "rotavirus infection" ,
-                    "rubella" ,
-                    "salmonellosis" ,
-                    "sars" ,
-                    "shigellosis" ,
-                    "smallpox" ,
-                    "staphylococcal enterotoxin b" ,
-                    "thypoid fever" ,
-                    "tuberculosis",
-                    "tularemia" ,
-                    "vaccinia and cowpox" ,
-                    "varicella" ,
-                    "west nile virus" ,
-                    "yellow fever" ,
-                    "yersiniosis" ,
-                    "zika" ,
-                    "legionares" ,
-                    "listeriosis" ,
-                    "monkeypox" ,
-                    "COVID-19" 
-                ], 
-                "syndrome": [
-                    "Haemorrhagic Fever" ,
-                    "Acute Flacid Paralysis" ,
-                    "Acute gastroenteritis" ,
-                    "Acute respiratory syndrome" ,
-                    "Influenza-like illness" ,
-                    "Acute fever and rash" ,
-                    "Fever of unknown Origin" ,
-                    "Encephalitis",
-                    "Meningitis"
-                ]
-            };
-
-            let diseases = []; 
-            let syndromes = []; 
-
-
-            for (let disease of Disease_and_syndrome_keywords.disease) {
-                //console.log(word)
-                if (article.textContent.includes(disease)) {
-                    diseases.push(disease); 
-                }
-            }
-            for (let syndrome of Disease_and_syndrome_keywords.syndrome) {
-                //console.log(word)
-                if (article.textContent.includes(syndrome)) {
-                    syndromes.push(syndrome); 
-                }
-            }
-            //console.log(diseases)
-            let reports = {
-                diseases: diseases, 
-                syndromes: syndromes,
-                event_date: event_date, 
-                locations: [], 
-            }
+            let reports = reportGenerator(article.textContent, dateOfPublication)
+            //console.log(reports)
 
             articles.push({
                 url: articleURL,
@@ -167,6 +63,7 @@ async function articleScraper(results) {
                 headline: headline,
                 main_text: mainText,
                 reports: reports,
+                //content: article
             });
         }
     });
@@ -185,11 +82,11 @@ async function articleScraper(results) {
     if (blockedArticles.length > 0) {
         articles.push(`${blockedArticles.length} article(s) could not be scraped from: ${JSON.stringify(blockedArticles)}`);
     }
-    return articles;
+    return JSON.stringify(articles, null, 2);
 }
 
 // only for testing, remove once working with api
-/*let testArray = [{
+let testArray = [{
     url: 'http://outbreaks.globalincidentmap.com/eventdetail.php?ID=13315',
     datePublished: '2013-05-12 04:00:00'
   },
@@ -204,8 +101,13 @@ async function articleScraper(results) {
   {
     url: 'http://outbreaks.globalincidentmap.com/eventdetail.php?ID=12525',
     datePublished: '2013-02-09 02:30:00'
-  }]
-articleScraper(testArray).then(console.log).catch(console.error);*/
+}]
+
+/*let testArray = [{
+    url: 'http://outbreaks.globalincidentmap.com/eventdetail.php?ID=43845',
+    datePublished: '2013-02-09 02:30:00'
+}]*/
+articleScraper(testArray).then(console.log).catch(console.error);
 
 export default articleScraper;
 

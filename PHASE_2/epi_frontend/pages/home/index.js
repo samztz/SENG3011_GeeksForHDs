48b44bd478673d0b7c372ff41667f6d183f16b71
Mashira Farid <mashira.farid@student.unsw.edu.au>
2022-04-04 20:30:40 +1000

    added ability to switch between map and graph view for dash home page

--- PHASE_2/epi_frontend/pages/home/index.js
+++ PHASE_2/epi_frontend/pages/home/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import DashNav from "../../components/dashNav";
 import { ThemeProvider } from '@mui/material/styles'
 import { theme } from "../../components/theme.js"
@@ -11,8 +12,26 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import ReportsTable from "../../components/reportsTable";
 import FilterDropdowns from "../../components/filterDropdowns";
 import Grid from '@mui/material/Grid';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 function DashHome() {
+  const [alignment, setAlignment] = React.useState('graph');
+  const [map, showMap] = React.useState(false);
+  const [graph, showGraph] = React.useState(true);
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+    // if map button clicked
+    if (newAlignment == 'map') {
+      showMap(true)
+      showGraph(false)
+    } else if (newAlignment == 'graph') {
+      showGraph(true)
+      showMap(false)
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
@@ -22,7 +41,7 @@ function DashHome() {
           sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
         >
           <Toolbar />
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
             <Card sx={{ mb: 2 }} width={'100%'}>
                 <CardContent style={{backgroundColor: theme.palette.white.main}}>
                 <Grid
@@ -33,12 +52,20 @@ function DashHome() {
                   alignItems="center"
                 >
                     <Grid item container direction="column" xs={6} md={9}>
-                      <ButtonGroup variant="outlined" aria-label="outlined button group" sx={{mb:2}}>
-                        <Button>Graphs</Button>
-                        <Button>Map</Button>
-                      </ButtonGroup>
+                      <ToggleButtonGroup
+                        color="secondary"
+                        value={alignment}
+                        exclusive
+                        onChange={handleChange}
+                        sx={{mb:2}}
+                      >
+                        <ToggleButton value="graph">Graph</ToggleButton>
+                        <ToggleButton value="map">Map</ToggleButton>
+                      </ToggleButtonGroup>
                     
-                    <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg" alt="map" width={'100%'} />
+                    {map ? 
+                    <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg" alt="map" width={'100%'} /> :
+                    <img src="https://www.howtogeek.com/wp-content/uploads/2021/11/GoogleSheetsLineChart.png?width=1198&trim=1,1&bg-color=000&pad=1,1" alt="graph" width={'100%'} />}
                     </Grid>
                   <Grid item container direction="column" alignItems="center" xs={6} md={3}>
                     <Typography variant="h5" fontWeight="fontWeightBold" sx={{mb:1}}>Filter By:</Typography>
--- PHASE_2/epi_frontend/pages/login/index.js
+++ PHASE_2/epi_frontend/pages/login/index.js
@@ -11,50 +11,50 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
 function Login() {
-  return (
-    <ThemeProvider theme={theme}>
-        <GlobalStyles styles={{ body: { backgroundColor: theme.palette.primary.main } }} />
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: '100vh' }}
-        >
-            <Grid item>
-                <Card>
-                <CardContent>
-                    <Typography variant="h3" color="primary" fontWeight="fontWeightBold" textAlign="center" sx={{mb:1}}>Log In</Typography>
-                    <TextField
-                        id="outlined-email-input"
-                        label="Email"
-                        type="email"
-                        autoComplete="current-email"
-                        margin="dense"
-                        fullWidth
-                    />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        margin="dense"
-                        fullWidth
-                        sx={{mb:1}}
-                    />
-                    <Grid container justifyContent={'center'}>
-                        <Button variant="outlined" color='secondary' sx={{m:1, mb:0}}>Cancel</Button>
-                        <Button variant="contained" color='secondary' sx={{m:1, mb:0}}>Log In</Button>
-                    </Grid>
-                </CardContent>
-            </Card>
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles styles={{ body: { backgroundColor: theme.palette.primary.main } }} />
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ minHeight: '100vh' }}
+            >
+                <Grid item>
+                    <Card>
+                    <CardContent>
+                        <Typography variant="h3" color="primary" fontWeight="fontWeightBold" textAlign="center" sx={{mb:1}}>Log In</Typography>
+                        <TextField
+                            id="outlined-email-input"
+                            label="Email"
+                            type="email"
+                            autoComplete="current-email"
+                            margin="dense"
+                            fullWidth
+                        />
+                        <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            margin="dense"
+                            fullWidth
+                            sx={{mb:1}}
+                        />
+                        <Grid container justifyContent={'center'}>
+                            <Button variant="outlined" color='secondary' sx={{m:1, mb:0}}>Cancel</Button>
+                            <Button variant="contained" color='secondary' sx={{m:1, mb:0}}>Log In</Button>
+                        </Grid>
+                    </CardContent>
+                </Card>
+                </Grid>
+                
             </Grid>
             
-        </Grid>
-        
-    </ThemeProvider>
-  );
+        </ThemeProvider>
+    );
 }
 
 export default Login 
